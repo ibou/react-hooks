6ba376bbd96dbb8ceaef6b681b8837b6da7d9d1e
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-05-27 04:28:04 +0200

    add reaction

---
+++ reaction/src/state/actions.js
@@ -0,0 +1,16 @@
+import { NEW_MESSAGE, SET_USERNAME } from './types';
+import { v4 as uuid } from 'uuid';
+
+export const newMessage = ({ text, username }) => ({
+    type: NEW_MESSAGE,
+    item: { id: uuid(), text, username, timestamp: Date.now() }
+});
+
+export const setUsername = username => ({
+    type: SET_USERNAME, username
+});
+
+export const createReaction = ({ type, emoji, username, messageId }) => ({
+    type,
+    item: { id: uuid(), timestamp: Date.now(), emoji, username, messageId }
+});
